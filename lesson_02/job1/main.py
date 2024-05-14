1d14bfcb7882c814a76bff57e9ad9c1cb9029cb1
CaligoLab <tanchikt3485@gmail.com>
2024-05-15 01:02:55 +0200

    job1 - create/re-create raw_dir

--- lesson_02/job1/main.py
+++ lesson_02/job1/main.py
@@ -25,7 +25,7 @@ def main() -> flask_typing.ResponseReturnValue:
 
     Proposed POST body in JSON:
     {
-      "data: "2022-08-09",
+      "date: "2022-08-09",
       "raw_dir": "/path/to/my_dir/raw/sales/2022-08-09"
     }
     """
