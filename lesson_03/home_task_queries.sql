872dc6e23ae08ecb0df3e9764104640dc10a8237
CaligoLab <tanchikt3485@gmail.com>
2024-05-23 19:03:01 +0200

    lesson_03 hw submit

---
+++ lesson_03/home_task_queries.sql
@@ -0,0 +1,75 @@
+/*
+ Завдання на SQL до лекції 03.
+ */
+
+
+/*
+1.
+Вивести кількість фільмів в кожній категорії.
+Результат відсортувати за спаданням.
+*/
+SELECT c.name AS category_name, COUNT(fc.film_id) AS movie_count
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+GROUP BY c.name
+ORDER BY movie_count DESC;
+
+
+/*
+2.
+Вивести 10 акторів, чиї фільми брали на прокат найбільше.
+Результат відсортувати за спаданням.
+*/
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(r.rental_id) AS rental_count
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY rental_count DESC
+LIMIT 10;
+
+
+
+/*
+3.
+Вивести категорія фільмів, на яку було витрачено найбільше грошей
+в прокаті
+*/
+SELECT c.name AS category_name, SUM(p.amount) AS total_amount
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+JOIN film f ON fc.film_id = f.film_id
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+JOIN payment p ON r.rental_id = p.rental_id
+GROUP BY c.name
+ORDER BY total_amount DESC
+LIMIT 1;
+
+
+/*
+4.
+Вивести назви фільмів, яких не має в inventory.
+Запит має бути без оператора IN ---- Чому?
+*/
+SELECT f.title
+FROM film f
+LEFT JOIN inventory i ON f.film_id = i.film_id
+WHERE i.film_id IS NULL;
+
+/*
+5.
+Вивести топ 3 актори, які найбільше зʼявлялись в категорії фільмів “Children”.
+*/
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(*) AS appearance_count
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Children'
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY appearance_count DESC
+LIMIT 3;
